be654368a193078ff236e1eab4026ba7f8366b6b
Justina Kuchta <justyna.kuchta@gmail.com>
2020-06-02 19:52:57 -0400

    completed practice algorithms - exercise 8

--- problem-solving-patterns/section6-exercises.js
+++ problem-solving-patterns/section6-exercises.js
@@ -146,5 +146,39 @@ console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)) //39
 console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)) // 5
 console.log(maxSubarraySum([2, 3], 3)) // null
 
+// EXERCISE 8 - MIN SUBARRAY LENGTH
+
+const minSubArrayLen = (nums, sum) => {
+    let total = 0
+    let start = 0
+    let end = 0
+    let minLen = Infinity
+
+    while (start < nums.length) {
+        // if current window does not add up to sum, move window to the right
+        if (total < sum && end < nums.length) {
+            total += nums[end]
+            end++
+        
+        }
+        // if current window adds up to at least the sum we can shrink the window 
+        else if (total >= sum) {
+            minLen = Math.min(minLen, end-start)
+            total -= nums[start]
+            start++
+        } else {
+            break
+        }
+    }
+    return minLen === Infinity ? 0 : minLen
+}
+
+console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)) // 2
+console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)) // 2
+console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)) // 1
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55), 39) // 3
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)) // 5
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95)) // 0
+
 
 
