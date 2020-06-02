999a9730df569828058bdac721c28b91a3caedb0
Justina Kuchta <justyna.kuchta@gmail.com>
2020-06-02 16:57:24 -0400

    completed practice algorithms - exercise 7

--- problem-solving-patterns/section6-exercises.js
+++ problem-solving-patterns/section6-exercises.js
@@ -124,3 +124,27 @@ const isSubsequence2 = (str1, str2) => {
     return isSubsequence2(str1, str2.slice(1))
 }
 
+// EXERCISE 7 - MAX SUBARRAY SUM
+const maxSubarraySum = (arr, num) => {
+    if (arr.length < num) return null
+    let max = 0
+    let temp = 0
+
+    for (let i = 0; i < num; i++) {
+        max += arr[i]
+    }
+    temp = max
+    for (let i = num; i < arr.length; i++) {
+        temp = temp - arr[i - num] + arr[i]
+        max = Math.max(temp, max)
+    }
+    return max
+}
+
+console.log(maxSubarraySum([100, 200, 300, 400], 2)) // 700
+console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)) //39
+console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)) // 5
+console.log(maxSubarraySum([2, 3], 3)) // null
+
+
+
