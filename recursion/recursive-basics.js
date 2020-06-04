e62f410b43e0befcf1510b1f87f4532d249cd618
Justina Kuchta <justyna.kuchta@gmail.com>
2020-06-04 14:14:32 -0400

    recursive helper methods

--- recursion/recursive-basics.js
+++ recursion/recursive-basics.js
@@ -1,3 +1,4 @@
+// REGULAR FACTORIAL (CALLS ITSELF)
 const countDown = num => {
     if (num <= 0) {
         console.log('done!')
@@ -26,3 +27,41 @@ const recursiveFactorial = num => {
     if(num === 1) return 1 // because factorial of 1 is 1
     return num * factorial(num - 1)
 }
+
+// RECURSION PITFALLS
+// 1. no base case or the base case is wrong
+// 2. forgetting to return
+// 3. stack overflow! - recursion not stopping
+
+// HELPER METHOD RECURSION (OUTER FUNCTION AND INSIDE A HELPER RECURSIVE FUNCTION)
+
+const outer = input => {
+    let outerScopeVariable = []
+
+    const helper = helperInput => {
+        helper(helperInput--)
+    }
+
+    helper(input)
+    return outerScopeVariable
+}
+
+// for example
+
+const collectOddValues = arr => {
+    let result = []
+
+    const helper = helperInput => {
+        if (helperInput.length === 0) {
+            return
+        }
+
+        if (helperInput[0] * 2 !== 0) {
+            result.push(helperInput[0])
+        }
+    }
+
+    helper(arr)
+    return result
+}
+
