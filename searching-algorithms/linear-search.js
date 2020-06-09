e5c39621069b473ecc751e60713a8d249178a5f6
Justina Kuchta <justyna.kuchta@gmail.com>
2020-06-09 13:37:26 -0400

    linear search algorithms

---
+++ searching-algorithms/linear-search.js
@@ -0,0 +1,7 @@
+let states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+
+// best way to find a particular state?
+// one way is to check one by one, which is not bad, but there are better ways
+
+// what if it is an unsorted list? We have to check one by one - LINEAR SEARCH
+// indexOf, includes, find, findIndex are all linear search algorithms
