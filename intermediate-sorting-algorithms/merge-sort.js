8489b0cce4a4b9ccb382ca9b4d62b9453702778d
Justina Kuchta <justyna.kuchta@gmail.com>
2020-06-11 14:15:33 -0400

    wrote mergesort

--- intermediate-sorting-algorithms/merge-sort.js
+++ intermediate-sorting-algorithms/merge-sort.js
@@ -23,11 +23,11 @@ const merge = (arr1, arr2) => {
     while (arr1.length > i && arr2.length > j) {
         if (arr1[i] < arr2[j]) {
             mergedArr.push(arr1[i])
-            // console.log(mergedArr)
+            console.log(mergedArr)
             i++
         } else {
             mergedArr.push(arr2[j])
-            // console.log(mergedArr)
+            console.log(mergedArr)
             j++
         }
     }
@@ -45,3 +45,18 @@ const merge = (arr1, arr2) => {
 }
 
 merge([1, 10, 50], [2, 14, 99, 100])
+
+// define mergeSort function that takes an array
+// keep splitting up into smaller arrays until length is one
+// call merge on all small arrays recursively
+
+const mergeSort = arr => {
+    // base case
+    if (arr.length <= 1) return arr
+    let mid = Math.floor(arr.length/2)
+    let left = mergeSort(arr.slice(0, mid))
+    let right = mergeSort(arr.slice(mid))
+    return merge(left, right)
+}
+
+console.log(mergeSort([45,2,90,1,60,100,3]))
