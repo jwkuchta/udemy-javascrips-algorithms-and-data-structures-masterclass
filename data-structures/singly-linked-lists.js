a6c9a309127fd0dc98ad7c2d406a8112564963a3
Justina Kuchta <justyna.kuchta@gmail.com>
2020-06-15 16:04:18 -0400

    defined pop() in the class

--- data-structures/singly-linked-lists.js
+++ data-structures/singly-linked-lists.js
@@ -32,17 +32,48 @@ class SinglyLinkedList {
             this.tail = this.head
         } else {
             // take the tail and add on to the end
-            this.tail.next = this.tail
+            this.tail.next = node
             // move the tail one spot over
             this.tail = node
         }
         this.length++
         return this
     }
+
+    pop() {
+        // remove tail and assign a new tail which means going through the whole lists (we do not have a tail.previous)
+        // we need to find second to last item and make that the tail. We need to traverse the list.
+        // if the list is empty return undefined
+        if (!this.head) return undefined
+
+        let current = this.head
+        let newTail = current
+
+        while (current.next) {
+            newTail = current
+            current = current.next
+            
+        }
+        this.tail = newTail
+        // we need to writing to sever the arrow, so it no longer points to anything
+        this.tail.next = null
+        this.length--
+        if (this.length === 0) {
+            this.head = null
+            this.tail = null
+        } 
+        return current
+    }
 }
 
 let list = new SinglyLinkedList()
 list.push('hi')
-console.log(list.first, list.head, list.tail)
+// console.log(list.first, list.head, list.tail)
 list.push('there')
-console.log(list, list.tail)
+// console.log(list, list.tail)
+list.push('kitty')
+list.push('are')
+list.push('you')
+list.push('hungry')
+list.push('today')
+console.log(list.pop())
