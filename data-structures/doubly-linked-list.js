d74ed6a8d9a4b4e666e48731ad2a0819e6ca6393
Justina Kuchta <justyna.kuchta@gmail.com>
2020-06-16 17:02:37 -0400

    wrote and tested push() method

--- data-structures/doubly-linked-list.js
+++ data-structures/doubly-linked-list.js
@@ -16,6 +16,20 @@ class DoublyLinkedList {
         this.tail = null
         this.length = 0
     }
+
+    push(value) {
+        let node = new Node(value)
+        if(this.length === 0) {
+            this.head = node
+            this.tail = node
+        } else {
+            this.tail.next = node
+            node.prev = this.tail
+            this.tail = node
+        }
+        this.length++
+        return this
+    }
 }
 
 
