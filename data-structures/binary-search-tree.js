2ea3da2e451908f01537858daa894676aba29bd4
Justina Kuchta <justyna.kuchta@gmail.com>
2020-06-20 14:48:24 -0400

    binary search tree insert() solution

--- data-structures/binary-search-tree.js
+++ data-structures/binary-search-tree.js
@@ -10,6 +10,35 @@ class BinarySearchTree {
     constructor() {
         this.root = null
     }
+
+    insert(value) {
+        let newNode = new Node(value)
+        if (this.root === null) {
+            this.root = newNode
+            return this  
+        } else {
+            let current = this.root
+            while (true) {
+                if (value === current.value) return undefined
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode
+                        return this
+                    } else {
+                        current = current.left
+                    }
+                } else if (value > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode
+                        return this
+                    } else {
+                        current = current.right
+                    }
+                }
+                // console.log(current)
+            }
+        }
+    }
 }
 
 let tree = new BinarySearchTree()
@@ -19,4 +48,7 @@ tree.root.left = new Node(7)
 tree.root.left.right = new Node(9)
 
 // console.log(tree)
-console.log(tree.root)
+// console.log(tree.root)
+
+tree.insert(6)
+console.log(tree.root.left.left)
