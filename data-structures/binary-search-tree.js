2a8c7a8f0e7aff247bbd9f244dcdf0fa03042c93
Justina Kuchta <justyna.kuchta@gmail.com>
2020-06-20 15:15:58 -0400

    added mine and his solution for find()

--- data-structures/binary-search-tree.js
+++ data-structures/binary-search-tree.js
@@ -15,29 +15,60 @@ class BinarySearchTree {
         let newNode = new Node(value)
         if (this.root === null) {
             this.root = newNode
-            return this  
-        } else {
-            let current = this.root
-            while (true) {
-                if (value === current.value) return undefined
-                if (value < current.value) {
-                    if (current.left === null) {
-                        current.left = newNode
-                        return this
-                    } else {
-                        current = current.left
-                    }
-                } else if (value > current.value) {
-                    if (current.right === null) {
-                        current.right = newNode
-                        return this
-                    } else {
-                        current = current.right
-                    }
+            return this
+        }
+        let current = this.root
+        while (true) {
+            if (current.value === value) return undefined
+            if (current.value > value) {
+                if (current.left === null) {
+                    current.left = newNode
+                    return this
+                } else {
+                    current = current.left
                 }
-                // console.log(current)
+            } else {
+                if (current.right === null) {
+                    current.right = newNode
+                    return this
+                } else {
+                    current = current.right
+                }
             }
         }
+    }
+
+    find(value) {
+        if (this.root === null) return false
+        let current = this.root
+        while (true) {
+            if (current.value === value) return true, current
+            if (current.value > value) {
+                if (current.left === null) return false
+                current = current.left
+            } else {
+                if (current.right === null) return false
+                current = current.right
+            }
+        }
+    }
+
+    // his solution: 
+    find2(value) {
+        if (this.root === null) return false
+        let current = this.root
+        let found = false
+        while (!found && current) {
+            if (value < current.value) {
+                current = current.left
+            } else if (value > current.value) {
+                current = current.right
+            } else {
+                found = true
+            }
+        }
+        if (!found) return false
+        return current
     }
 }
 
@@ -51,4 +82,12 @@ tree.root.left.right = new Node(9)
 // console.log(tree.root)
 
 tree.insert(6)
-console.log(tree.root.left.left)
+console.log('these should be true')
+console.log(tree.find2(10))
+console.log(tree.find2(15))
+console.log(tree.find2(7))
+console.log(tree.find2(9))
+console.log('these should be false')
+console.log(tree.find2(72))
+console.log(tree.find2(90))
+console.log(tree.find2(100))
