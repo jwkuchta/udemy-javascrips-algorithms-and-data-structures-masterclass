2c776b7e155d53e7263c4cc882a0b075ee89e6bf
Justina Kuchta <justyna.kuchta@gmail.com>
2020-06-23 16:53:33 -0400

    wrote set and get methods for HashTables class

--- data-structures/hash-tables.js
+++ data-structures/hash-tables.js
@@ -1,37 +1,62 @@
-// hash tables are used to store key value pairs. The keys are not ordered.
-// they are used commonly because of their speed
-// we will implement our own hash table
+class HashTable {
+    constructor(size = 53) {
+        this.keyMap = new Array(size)
+    }
 
-// what makes a good hash function?
-// 1. it's fast (i.e. constant time)
-// 2. it distributes things uniformly
-// 3. it is deterministic (we get the same output per one given input)
+    _hash(key) {
+        let total = 0
+        let WEIRD_PRIME = 31
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i]
+            let value = char.charCodeAt(0) - 96
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length
+        }
+        return total
+    }
 
-// basic hashing function
-
-const hash = (key, arrLen) => {
-    let total = 0
-    for (let char of key) {
-        let value = char.charCodeAt(0) - 96
-        total = (total + value) % arrLen
+    set(key, value) {
+        let index = this._hash(key)
+        if (!this.keyMap[index]) {
+            this.keyMap[index] = []
+        }
+        this.keyMap[index].push([key, value])
+    }
+    
+    get(key) {
+        let index = this._hash(key)
+        // console.log(index)
+        if (this.keyMap[index]) {
+            if (this.keyMap[index].length === 1) {
+                console.log(this.keyMap[index])
+                return this.keyMap[index][1]
+            } else {
+                for (let i = 0; i < this.keyMap[index].length; i++) {
+                    console.log(i)
+                    if (this.keyMap[index][i][0] === key) {
+                        return this.keyMap[index][i]
+                    }
+                }
+            }
+            // return sthis.keyMap[index][1]
+        }
+        return undefined
     }
 }
 
-// problems?
-// only hashes strings
-// not constant time
-// not really random
-// a prime number arrLen reduces the number of colitions drastically
-
-const betterHash = (key, arrLen) => {
-    let total = 0
-    // prime numbers often used in hashes
-    let WEIRD_PRIME = 31
-    // loop key.length times or max 100
-    for (let i = 0; i < Math.min(key.length, 100); i++) {
-        let char = key[i]
-        let value = char.charCodeAt(0) -96
-        total = (total * WEIRD_PRIME + value) % arrLen
-    }
-    return total
-}
+let hash1 = new HashTable(3)
+hash1.set('red', 'apple')
+hash1.set('yellow', 'banana')
+hash1.set('green', 'onion')
+hash1.set('blue', 'cheese')
+hash1.set('white', 'mushrooms')
+hash1.set('purple', 'berries')
+hash1.set('orange', 'oranges')
+hash1.set('black', 'currants')
+console.log(hash1.get('red'))
+console.log(hash1.get('yellow'))
+console.log(hash1.get('green'))
+console.log(hash1.get('blue'))
+console.log(hash1.get('white'))
+console.log(hash1.get('purple'))
+console.log(hash1.get('orange'))
+console.log(hash1.get('black'))
