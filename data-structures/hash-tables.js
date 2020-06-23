90d7826c34ae6e842d362c946e1a9faedc31418c
Justina Kuchta <justyna.kuchta@gmail.com>
2020-06-23 17:31:17 -0400

    added my solutions for keys() and values()

--- data-structures/hash-tables.js
+++ data-structures/hash-tables.js
@@ -1,3 +1,5 @@
+const { generateKeyPairSync } = require("crypto")
+
 class HashTable {
     constructor(size = 53) {
         this.keyMap = new Array(size)
@@ -21,25 +23,6 @@ class HashTable {
         }
         this.keyMap[index].push([key, value])
     }
-    
-    // his solution
-    // get(key) {
-    //     let index = this._hash(key)
-    //     if (this.keyMap[index]) {
-    //         if (this.keyMap[index].length === 1) {
-    //             console.log(this.keyMap[index])
-    //             return this.keyMap[index][1]
-    //         } else {
-    //             for (let i = 0; i < this.keyMap[index].length; i++) {
-    //                 console.log(i)
-    //                 if (this.keyMap[index][i][0] === key) {
-    //                     return this.keyMap[index][i][1]
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return undefined
-    // }
 
     // my solution
     get(key) {
@@ -59,6 +42,40 @@ class HashTable {
         }
         return undefined
     }
+
+    keys() {
+        let keys = []
+        if (this.keyMap.length === 0) return null
+        if (this.keyMap.length === 1) {
+            keys.push(this.keyMap[0][0])
+        } else {
+            for (let arr of this.keyMap) {
+                for (let subArr of arr) {
+                    keys.push(subArr[0])
+                }
+            }
+        }
+        return keys
+    }
+
+    // beware of duplicates!
+    values() {
+        let values = []
+        if (this.keyMap.length === 0) return null
+        if (this.keyMap.length === 1) {
+            keys.push(this.keyMap[0][1])
+        } else {
+            for (let arr of this.keyMap) {
+                for (let subArr of arr) {
+                    values.push(subArr[1])
+                }
+            }
+        }
+        values = [...new Set(values)]
+        // this would also work but longer, so I prefer Set:
+        // values = values.filter((item, index) => values.indexOf(item) === index)
+        return values
+    }
 }
 
 let hash1 = new HashTable(3)
@@ -70,11 +87,15 @@ hash1.set('white', 'mushrooms')
 hash1.set('purple', 'berries')
 hash1.set('orange', 'oranges')
 hash1.set('black', 'currants')
-console.log(hash1.get('red'))
-console.log(hash1.get('yellow'))
-console.log(hash1.get('green'))
-console.log(hash1.get('blue'))
-console.log(hash1.get('white'))
-console.log(hash1.get('purple'))
-console.log(hash1.get('orange'))
-console.log(hash1.get('black'))
+hash1.set('yellow', 'currants')
+hash1.set('red', 'currants')
+// console.log(hash1.get('red'))
+// console.log(hash1.get('yellow'))
+// console.log(hash1.get('green'))
+// console.log(hash1.get('blue'))
+// console.log(hash1.get('white'))
+// console.log(hash1.get('purple'))
+// console.log(hash1.get('orange'))
+// console.log(hash1.get('black'))
+// console.log(hash1.keys())
+console.log(hash1.values())
