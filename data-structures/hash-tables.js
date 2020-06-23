087128e7106b142d64afed35080b74ee3e2c0eea
Justina Kuchta <justyna.kuchta@gmail.com>
2020-06-23 17:51:08 -0400

    summary and cleanup

--- data-structures/hash-tables.js
+++ data-structures/hash-tables.js
@@ -1,4 +1,8 @@
-const { generateKeyPairSync } = require("crypto")
+// Big O of a hash depends of how good the hashing function is and 
+// it is better to use the built in function than to write your own
+// a good hashing algorithms is O(n)
+
+// closer to Maps in JavaScript, dictionaries in Python
 
 class HashTable {
     constructor(size = 53) {
@@ -46,13 +50,9 @@ class HashTable {
     keys() {
         let keys = []
         if (this.keyMap.length === 0) return null
-        if (this.keyMap.length === 1) {
-            keys.push(this.keyMap[0][0])
-        } else {
-            for (let arr of this.keyMap) {
-                for (let subArr of arr) {
-                    keys.push(subArr[0])
-                }
+        for (let arr of this.keyMap) {
+            for (let subArr of arr) {
+                keys.push(subArr[0])
             }
         }
         return keys
@@ -62,13 +62,9 @@ class HashTable {
     values() {
         let values = []
         if (this.keyMap.length === 0) return null
-        if (this.keyMap.length === 1) {
-            keys.push(this.keyMap[0][1])
-        } else {
-            for (let arr of this.keyMap) {
-                for (let subArr of arr) {
-                    values.push(subArr[1])
-                }
+        for (let arr of this.keyMap) {
+            for (let subArr of arr) {
+                values.push(subArr[1])
             }
         }
         values = [...new Set(values)]
@@ -87,7 +83,6 @@ hash1.set('white', 'mushrooms')
 hash1.set('purple', 'berries')
 hash1.set('orange', 'oranges')
 hash1.set('black', 'currants')
-hash1.set('yellow', 'currants')
 hash1.set('red', 'currants')
 // console.log(hash1.get('red'))
 // console.log(hash1.get('yellow'))
@@ -97,5 +92,5 @@ hash1.set('red', 'currants')
 // console.log(hash1.get('purple'))
 // console.log(hash1.get('orange'))
 // console.log(hash1.get('black'))
-// console.log(hash1.keys())
+console.log(hash1.keys())
 console.log(hash1.values())
